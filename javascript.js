370201c196660f6bc07decd522e8cc8a14272114
bienking <bienking@hotmail.com>
2019-12-02 17:56:08 +0000

    Cities pages updated
    entry + form draft created

--- javascript.js
+++ javascript.js
@@ -27,3 +27,14 @@ function showSlides() {
 window.onload=function(){
     showSlides();
     }
+
+
+<script>
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
+</script>
